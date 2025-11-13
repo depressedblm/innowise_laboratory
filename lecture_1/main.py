45864ba5fc6ef6955787162c14267db5cf53221b
depressedblm <stanislav_zaitsev06@mail.ru>
2025-11-14 02:54:20 +0300

    Update main.py

--- lecture_1/main.py
+++ lecture_1/main.py
@@ -1,1 +1,14 @@
+from colorama import Fore, Back, Style, init
 
+# Инициализация colorama для Windows
+init(autoreset=True)
+
+def main():
+    print(Fore.RED + "Это красный текст")
+    print(Fore.GREEN + "Это зеленый текст")
+    print(Back.YELLOW + "Это текст с желтым фоном")
+    print(Style.BRIGHT + "Это яркий текст")
+    print(Fore.BLUE + Back.WHITE + "Синий текст на белом фоне")
+
+if __name__ == "__main__":
+    main()
